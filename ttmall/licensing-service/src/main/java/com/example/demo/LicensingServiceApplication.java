5edefaf89afed26b1e6f38900b88f3a2c5332445
fuying.jin <fuying.jin@nuance.com>
2018-06-13 11:30:25 -0400

    change to MySql DB;

--- ttmall/licensing-service/src/main/java/com/example/demo/LicensingServiceApplication.java
+++ ttmall/licensing-service/src/main/java/com/example/demo/LicensingServiceApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 public class LicensingServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("retry image push...");
 		SpringApplication.run(LicensingServiceApplication.class, args);
 	}
 }
