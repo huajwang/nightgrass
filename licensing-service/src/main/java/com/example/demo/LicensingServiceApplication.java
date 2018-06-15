1cd02cafb3408c162fbff4792302e8c2569a8822
Wang <huajian_wang@nuance.com>
2018-06-15 14:50:01 -0400

    enable eureka client annotation;

--- licensing-service/src/main/java/com/example/demo/LicensingServiceApplication.java
+++ licensing-service/src/main/java/com/example/demo/LicensingServiceApplication.java
@@ -3,13 +3,14 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @RefreshScope
+@EnableEurekaClient
 public class LicensingServiceApplication {
 
 	public static void main(String[] args) {
-		System.out.println("new change...");
 		SpringApplication.run(LicensingServiceApplication.class, args);
 	}
 }
