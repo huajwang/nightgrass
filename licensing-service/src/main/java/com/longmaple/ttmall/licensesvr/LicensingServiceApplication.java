850975baf00269023b6d5bc6ba3e2c05bd5925cb
Wang <huajian_wang@nuance.com>
2018-06-19 13:14:48 -0400

    attach UserContextInterceptor to the RestTemplate;

--- licensing-service/src/main/java/com/longmaple/ttmall/licensesvr/LicensingServiceApplication.java
+++ licensing-service/src/main/java/com/longmaple/ttmall/licensesvr/LicensingServiceApplication.java
@@ -1,5 +1,8 @@
 package com.longmaple.ttmall.licensesvr;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
@@ -9,16 +12,28 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import com.longmaple.ttmall.licensesvr.utils.UserContextInterceptor;
+
 @SpringBootApplication
 @EnableEurekaClient
 @EnableCircuitBreaker
 @EnableFeignClients
 public class LicensingServiceApplication {
-	
+
 	@LoadBalanced
 	@Bean
 	public RestTemplate restTemplate() {
-		return new RestTemplate();
+		
+		RestTemplate template = new RestTemplate();
+		List interceptors = template.getInterceptors();
+		if (interceptors == null){
+			template.setInterceptors(Collections.singletonList(new UserContextInterceptor()));
+		}
+		else{
+			interceptors.add(new UserContextInterceptor());
+			template.setInterceptors(interceptors);
+		}
+		return template;
 	}
 
 	public static void main(String[] args) {
