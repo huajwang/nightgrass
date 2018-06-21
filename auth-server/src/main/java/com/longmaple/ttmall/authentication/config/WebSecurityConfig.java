82d1c4c11233f13477587842ecf64090a88cbe0d
huajwang <cse63152@gmail.com>
2018-06-21 09:20:09 -0400

    add docker config for authentication service;

---
+++ auth-server/src/main/java/com/longmaple/ttmall/authentication/config/WebSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.longmaple.ttmall.authentication.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		 http
+		 .antMatcher("/**").authorizeRequests()
+		 .antMatchers("/", "/login**")
+		 .permitAll()
+		 .anyRequest().authenticated().and().formLogin().permitAll();
+	}
+	
+}
