969e6d8c5374be20a822f4d4bc9d99fab873a529
huajwang <cse63152@gmail.com>
2018-07-07 13:32:47 -0400

    add custom login and authorize page;

--- auth-server/src/main/java/com/longmaple/ttmall/authentication/AuthServerApplication.java
+++ auth-server/src/main/java/com/longmaple/ttmall/authentication/AuthServerApplication.java
@@ -1,12 +1,34 @@
 package com.longmaple.ttmall.authentication;
 
+import java.security.Principal;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class AuthServerApplication {
+@SessionAttributes("authorizationRequest")
+@Controller
+public class AuthServerApplication implements WebMvcConfigurer{
+	
+	@RequestMapping("/user")
+	@ResponseBody
+	public Principal user(Principal user) {
+		return user;
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/login").setViewName("login");
+		registry.addViewController("/oauth/confirm_access").setViewName("authorize");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthServerApplication.class, args);
--- auth-server/src/main/java/com/longmaple/ttmall/authentication/config/WebSecurityConfig.java
+++ auth-server/src/main/java/com/longmaple/ttmall/authentication/config/WebSecurityConfig.java
@@ -4,11 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Bean
@@ -18,11 +16,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		 http
-		 .antMatcher("/**").authorizeRequests()
-		 .antMatchers("/", "/login**")
-		 .permitAll()
-		 .anyRequest().authenticated().and().formLogin().permitAll();
+		http
+		.formLogin().loginPage("/login").permitAll()
+		.and()
+		.requestMatchers().antMatchers("/login", "/logout", "/oauth/authorize", "/oauth/confirm_access")
+		.and()
+		.authorizeRequests().anyRequest().authenticated();
 	}
-	
+
 }
