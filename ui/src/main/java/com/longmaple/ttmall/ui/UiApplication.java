6e5796691014b8300efe8c9a31523f412c87760f
huajwang <cse63152@gmail.com>
2018-07-09 17:50:46 -0400

    update ui project;

--- ui/src/main/java/com/longmaple/ttmall/ui/UiApplication.java
+++ ui/src/main/java/com/longmaple/ttmall/ui/UiApplication.java
@@ -1,15 +1,11 @@
-package demo;
+package com.longmaple.ttmall.ui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @SpringBootApplication
@@ -17,21 +13,19 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 @EnableOAuth2Sso
 public class UiApplication extends WebSecurityConfigurerAdapter {
 
-    public static void main(String[] args) {
-        SpringApplication.run(UiApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(UiApplication.class, args);
+	}
 
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        // @formatter:off
-        http
-        .logout().logoutSuccessUrl("/").and()
-            .authorizeRequests()
-                .antMatchers("/index.html", "/", "/login").permitAll()
-                .anyRequest().authenticated()
-                .and()
-            .csrf()
-                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
-            // @formatter:on
-    }
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http
+		.logout().logoutSuccessUrl("/").and()
+		.authorizeRequests()
+		.antMatchers("/index.html", "/", "/login").permitAll()
+		.anyRequest().authenticated()
+		.and()
+		.csrf()
+		.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+	}
 }
