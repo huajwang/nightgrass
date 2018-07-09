7b705d113aaba8aac87bb6d192d74878644db079
huajwang <cse63152@gmail.com>
2018-07-09 17:44:32 -0400

    remove unused project;

--- frontier/src/main/java/com/longmaple/ttmall/frontier/data/EMallUserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.longmaple.ttmall.frontier.data;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface EMallUserRepo extends CrudRepository<EMallUser, Long> {
-
-	EMallUser findByUsername(String username);
-
-}
