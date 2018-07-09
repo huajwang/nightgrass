e9675bd9277804b3ed2b0fb019cfa47a30d4042d
huajwang <cse63152@gmail.com>
2018-07-09 17:42:06 -0400

    At this resource server, define feign interceptor so that the feign client can do token relay to surrounding services;

--- licensing-service/src/main/java/com/longmaple/ttmall/licensesvr/LicensingServiceApplication.java
+++ licensing-service/src/main/java/com/longmaple/ttmall/licensesvr/LicensingServiceApplication.java
@@ -5,12 +5,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import com.longmaple.ttmall.licensesvr.client.UserFeignClientInterceptor;
+
+import feign.RequestInterceptor;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableCircuitBreaker
 @EnableFeignClients
 public class LicensingServiceApplication {
+	
+	@Bean
+    public RequestInterceptor getUserFeignClientInterceptor() {
+        return new UserFeignClientInterceptor();
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(LicensingServiceApplication.class, args);
