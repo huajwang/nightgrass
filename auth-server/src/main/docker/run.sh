34f91401055698f2a9e43a3a7e1a337e964bc04d
fuying.jin <fuying.jin@nuance.com>
2018-07-20 15:00:22 -0400

    update project pom.xml; remove database startup check;

--- auth-server/src/main/docker/run.sh
+++ auth-server/src/main/docker/run.sh
@@ -7,12 +7,6 @@ echo "******* Eureka Server has started"
 
 
 echo "********************************************************"
-echo "Waiting for the database server to start on port $DATABASE_PORT"
-echo "********************************************************"
-while ! `nc -z database $DATABASE_PORT`; do sleep 3; done
-echo "******** Database Server has started "
-
-echo "********************************************************"
 echo "Waiting for the configuration server to start on port $CONFIGSERVER_PORT"
 echo "********************************************************"
 while ! `nc -z configserver $CONFIGSERVER_PORT`; do sleep 3; done
