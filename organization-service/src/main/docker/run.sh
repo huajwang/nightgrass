a166089f9f5b22024d7c168fef5e5c176e5b44f2
Huajian Wang <yc_whj@hotmail.com>
2018-07-29 19:30:46 -0400

    make service to wait until authentication sever starts up as the projects need oauth/token_key for public key of auth server;

--- organization-service/src/main/docker/run.sh
+++ organization-service/src/main/docker/run.sh
@@ -1,8 +1,3 @@
-#!/bin/sh
-getPort() {
-    echo $1 | cut -d : -f 3 | xargs basename
-}
-
 echo "********************************************************"
 echo "Waiting for the eureka server to start on port $EUREKASERVER_PORT"
 echo "********************************************************"
@@ -11,16 +6,16 @@ echo "******* Eureka Server has started"
 
 
 echo "********************************************************"
-echo "Waiting for the database server to start on port $DATABASESERVER_PORT"
-echo "********************************************************"
-while ! `nc -z database $DATABASESERVER_PORT`; do sleep 3; done
-echo "******** Database Server has started "
-
-echo "********************************************************"
 echo "Waiting for the configuration server to start on port $CONFIGSERVER_PORT"
 echo "********************************************************"
 while ! `nc -z configserver $CONFIGSERVER_PORT`; do sleep 3; done
 echo "*******  Configuration Server has started"
+
+echo "********************************************************"
+echo "Waiting for the authorization server to start on port $AUTHSERVER_PORT"
+echo "********************************************************"
+while ! `nc -z authenticationservice $AUTHSERVER_PORT`; do sleep 3; done
+echo "*******  Authorization Server has started"
 
 
 echo "********************************************************"
