be060e383c323bf0d0ada5efaa9163c790c50f68
Huajian Wang <yc_whj@hotmail.com>
2018-07-28 20:29:25 -0400

    1. remove explicit version number from pom.xml to resolve jar conflict; 2. remove the executeion e2e from ui as it needs chrome web driver;

--- ui/src/main/docker/run.sh
+++ ui/src/main/docker/run.sh
@@ -16,4 +16,4 @@ echo "********************************************************"
 java -Djava.security.egd=file:/dev/./urandom -Dserver.port=$SERVER_PORT   \
      -Deureka.client.serviceUrl.defaultZone=$EUREKASERVER_URI             \
      -Dspring.cloud.config.uri=$CONFIGSERVER_URI                          \
--Dspring.profiles.active=$PROFILE -jar /usr/local/licensingservice/@project.build.finalName@.jar
+-Dspring.profiles.active=$PROFILE -jar /usr/local/ui/@project.build.finalName@.jar
