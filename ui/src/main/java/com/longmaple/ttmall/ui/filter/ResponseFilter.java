72ee19354fcd7dded9333db15682bbecc8db1760
huajwang <cse63152@gmail.com>
2019-02-02 11:30:58 -0500

    Add trace ID to Zuul HTTP response header;

--- ui/src/main/java/com/longmaple/ttmall/ui/filter/ResponseFilter.java
+++ ui/src/main/java/com/longmaple/ttmall/ui/filter/ResponseFilter.java
@@ -2,19 +2,25 @@ package com.longmaple.ttmall.ui.filter;
 
 
 import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ResponseFilter extends ZuulFilter{
-    private static final int  FILTER_ORDER=1;
-    private static final boolean  SHOULD_FILTER=true;
+public class ResponseFilter extends ZuulFilter {
+	
+    private static final int  FILTER_ORDER = 1;
+    private static final boolean  SHOULD_FILTER = true;
     private static final Logger logger = LoggerFactory.getLogger(ResponseFilter.class);
     
     @Autowired
     FilterUtils filterUtils;
+    @Autowired
+    Tracer tracer;
 
     @Override
     public String filterType() {
@@ -34,6 +40,9 @@ public class ResponseFilter extends ZuulFilter{
     @Override
     public Object run() {
     	logger.debug("Zuul response filter...");
+    	RequestContext requestContext = RequestContext.getCurrentContext();
+    	requestContext.getResponse().addHeader(FilterUtils.CORRELATION_ID,
+    			tracer.getCurrentSpan().traceIdString());
         return null;
     }
 }
