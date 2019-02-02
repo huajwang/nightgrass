4226b917feb2f38eebb08e3316f5f55b64d66ace
Huajian Wang <cse63152@gamil.com>
2019-02-02 18:53:27 -0500

    Add sleuth and zipkin for auth-server project;

--- auth-server/src/main/docker/run.sh
+++ auth-server/src/main/docker/run.sh
@@ -20,4 +20,5 @@ java -Djava.security.egd=file:/dev/./urandom -Dserver.port=$SERVER_PORT   \
      -Deureka.client.serviceUrl.defaultZone=$EUREKASERVER_URI             \
      -Dspring.cloud.config.uri=$CONFIGSERVER_URI                          \
      -Dspring.profiles.active=$PROFILE                                   \
+     -Dspring.zipkin.baseUrl=$ZIPKIN_URI \
      -jar /usr/local/authenticationservice/@project.build.finalName@.jar
