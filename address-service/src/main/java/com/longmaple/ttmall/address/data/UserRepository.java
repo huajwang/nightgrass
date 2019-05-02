8e472445dab68436b2a87022459a81b9a08fd364
Huajian Wang <cse63152@gmail.com>
2019-05-02 19:50:17 -0400

    Remove Gradle project: address-service

--- address-service/src/main/java/com/longmaple/ttmall/address/data/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.longmaple.ttmall.address.data;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<EMallUser, Long> {
-
-	EMallUser findByUsername(String username);
-}
